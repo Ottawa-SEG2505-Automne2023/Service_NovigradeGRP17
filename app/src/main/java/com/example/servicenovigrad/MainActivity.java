736d63463eaf53d76aca19deee8f80929c27b069
Yahya-bensalih <145401070+Yahya-bensalih@users.noreply.github.com>
2023-10-29 05:41:35 +0100

    update

--- app/src/main/java/com/example/servicenovigrad/MainActivity.java
+++ app/src/main/java/com/example/servicenovigrad/MainActivity.java
@@ -17,100 +17,116 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    //Declaration des objets de l'activité
+
+    // Déclaration des composants UI
     Button register;
     Button login;
     TextView Welcome;
     TextView loginOrSignup;
     EditText username;
     EditText password;
+    // Déclaration de la base de données Firebase
     FirebaseDatabase baseDonnees;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //initialisation des objets d'instance
-        username=(EditText) findViewById(R.id.username);
-        password=(EditText) findViewById(R.id.password);
-        register=(Button) findViewById(R.id.register);
-        login=(Button) findViewById(R.id.login);
-        Welcome=(TextView) findViewById(R.id.Welcome);
-        loginOrSignup =(TextView) findViewById(R.id.loginOrSignup);
-        baseDonnees=FirebaseDatabase.getInstance("https://servicenovigrad-9d027-default-rtdb.firebaseio.com");
-        //On click du bouton de connexion, qui lance une tentative de connexion avec le username et password dans les edit text
+
+        // Initialisation des composants UI
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        register = (Button) findViewById(R.id.register);
+        login = (Button) findViewById(R.id.login);
+        Welcome = (TextView) findViewById(R.id.Welcome);
+        loginOrSignup = (TextView) findViewById(R.id.loginOrSignup);
+
+        // Initialisation de la base de données Firebase
+        baseDonnees = FirebaseDatabase.getInstance("https://servicenovigrad-9d027-default-rtdb.firebaseio.com");
+
+        // Gestion de l'événement de clic pour le bouton de connexion
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
+                // Vérification de la non-vacuité des champs
                 if(champNonVide(username.getText().toString()) && champNonVide(password.getText().toString())){
+                    // Tentative de connexion
                     tentative(username.getText().toString(), password.getText().toString());
+                } else {
+                    // Affichage d'un message si les champs sont vides
+                    Toast.makeText(getApplicationContext(), "SVP remplissez tout les champs", Toast.LENGTH_LONG).show();
                 }
-                else{Toast.makeText(getApplicationContext(), "SVP remplissez tout les champs", Toast.LENGTH_LONG).show();}
             }
         });
-        //On click du bouton d'inscription, qui envois directement à l'activité d'inscription
+
+        // Gestion de l'événement de clic pour le bouton d'inscription
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
+                // Transition vers l'activité d'inscription
                 Intent intent2 = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent2);
             }
         });
-
-
     }
+
+    // Méthode pour vérifier si un texte est non vide
     public static boolean champNonVide(String texte){
-        if (texte==null || texte.equals("")){
+        if (texte == null || texte.equals("")){
             return false;
+        } else {
+            return true;
         }
-        else{return true;}
     }
-    //methode appelee lors de la tentative de connexion, qui comparer si le mot de passe correspond a celui stocke dans la base
-    //de donnees, et si c'est correspondant, cree un intent et passe le nom et le role de l'utilisateur en extra pour l'activité prochaine
+
+    // Méthode pour gérer la tentative de connexion
     private void tentative(String username, String password){
-        DatabaseReference refBase=FirebaseDatabase.getInstance().getReference();
+        DatabaseReference refBase = FirebaseDatabase.getInstance().getReference();
         DatabaseReference refPass = refBase.child("users").child(username).child("mdp");
         DatabaseReference refRole = refBase.child("users").child(username).child("role");
+
+        // Vérification du mot de passe dans la base de données
         refPass.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String pass = dataSnapshot.getValue(String.class);
-                if(pass!=null && pass.equals(password)) {
+                if (pass != null && pass.equals(password)) {
+                    // Si le mot de passe est correct, récupération du rôle
                     refRole.addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
                             String role = dataSnapshot.getValue(String.class);
-                            if(role.equals("Administrateur")){
+                            // En fonction du rôle, redirection vers l'activité appropriée
+                            if (role.equals("Administrateur")) {
                                 Intent intent0 = new Intent(MainActivity.this, LoggedIn.class);
                                 intent0.putExtra("nom", username);
                                 startActivity(intent0);
-                            }
-                            else if(role.equals("Employe")) {
-                                Intent intent1 = new Intent(MainActivity.this,LoggedIn.class);
+                            } else if (role.equals("Employe")) {
+                                Intent intent1 = new Intent(MainActivity.this, LoggedIn.class);
                                 intent1.putExtra("nom", username);
                                 startActivity(intent1);
-                            }
-                            else{
-                                Intent intent2 = new Intent(MainActivity.this,LoggedIn.class);
+                            } else {
+                                Intent intent2 = new Intent(MainActivity.this, LoggedIn.class);
                                 intent2.putExtra("nom", username);
                                 startActivity(intent2);
                             }
                         }
+
                         @Override
                         public void onCancelled(DatabaseError databaseError) {
+                            // Gestion de l'erreur si nécessaire
                         }
                     });
+                } else {
+                    // Si le mot de passe est incorrect, affichage d'un message d'erreur
+                    Toast.makeText(getApplicationContext(), "Nom d'utilisateur/Mot de passe incorrect ou champs vides", Toast.LENGTH_LONG).show();
                 }
-                else{Toast.makeText(getApplicationContext(),"Nom d'utilisateur/Mot de passe incorrect ou champs vides", Toast.LENGTH_LONG).show();}
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
+                // Gestion de l'erreur si nécessaire
             }
         });
-
-
-
     }
 }
